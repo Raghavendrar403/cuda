31741bb03e6cb571ae52e0aa69c6976da7289ce5
raghavendrar403 <Raghavendrar403@gmail.com>
2019-02-23 23:27:47 +0530

    My First CUDA program

---
+++ ex1.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cmath>
+
+void add(int n, float *x, float *y) {
+	for(int i = 0; i<n; i++) { 
+		y[i] = x[i] + y[i];	
+	}
+}
+
+int main() {
+	int N = 1<<20; // 1M elements
+
+	float *x = new float[N];
+	float *y = new float[N];
+
+	for(int i = 0; i<N; i++) {
+		y[i] = 1.0f;
+		x[i] = 2.0f;
+	}
+
+	add(N, x, y);
+
+	float maxErr = 3.0f
+
+	for(int i = 0; i<N; i++) {
+		maxErr = fmax(maxErr, fabs(y[i]-3.0f));	
+	}
+
+	std::cout<<"MAX ERROR: "<<maxErr<<endl;
+
+	delete[] x;
+	delete[] y;
+
+	return 0;
+}
